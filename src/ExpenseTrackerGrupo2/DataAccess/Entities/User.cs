fc396277ea7ca0b97fea5d6d94b01f42d1d02bb5
salet-gutierrez-jalafund <salet.gutierrez@fundacion-jala.org>
2024-09-17 16:31:42 -0400

    feat: add entity User

---
+++ src/ExpenseTrackerGrupo2/DataAccess/Entities/User.cs
@@ -0,0 +1,42 @@
+using ExpenseTrackerGrupo2.src.ExpenseTrackerGrupo2.DataAccess.Entities.Interfaces;
+
+namespace ExpenseTrackerGrupo2.src.ExpenseTrackerGrupo2.DataAccess.Entities;
+
+public class User : IEntity
+{
+    private Guid _id;
+    private string _name;
+    private string _email;
+    private string _passwordHash;
+    private DateTime _createdAt;
+
+    public required Guid Id
+    {
+        get => _id;
+        set => _id = value;
+    }
+
+    public required string Name
+    {
+        get => _name;
+        set => _name = value;
+    }
+
+    public required string Email
+    {
+        get => _email;
+        set => _email = value;
+    }
+
+    public required string PasswordHash
+    {
+        get => _passwordHash;
+        set => _passwordHash = value;
+    }
+
+    public required DateTime CreatedAt
+    {
+        get => _createdAt;
+        set => _createdAt = value;
+    }
+}
