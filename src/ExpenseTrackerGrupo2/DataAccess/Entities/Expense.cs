0f661b346036780881e0197d6866bc61fd6ff165
salet-gutierrez-jalafund <salet.gutierrez@fundacion-jala.org>
2024-09-22 17:32:49 -0400

    refactor: add attributes of the Expense entity

--- src/ExpenseTrackerGrupo2/DataAccess/Entities/Expense.cs
+++ src/ExpenseTrackerGrupo2/DataAccess/Entities/Expense.cs
@@ -2,43 +2,12 @@ namespace ExpenseTrackerGrupo2.src.ExpenseTrackerGrupo2.DataAccess.Entities;
 
 public class Expense
 {
-    private Guid _id;
-    private Guid _userId;
-    private decimal _amount;
-    private DateTime _date;
-    private DateTime _createdAt;
-
-    public required Guid Id
-    {
-        get => _id;
-        set => _id = value;
-    }
-
-    public required Guid UserId
-    {
-        get => _userId;
-        set => _userId = value;
-    }
-
-    public required decimal Amount
-    {
-        get => _amount;
-        set => _amount = value;
-    }
-
+    public Guid Id { get; init; } = Guid.NewGuid();
+    public required decimal Amount { get; set; }
     public required string Description { get; set; }
-
     public required string Category { get; set; }
+    public required DateTime Date;
+    public DateTime CreatedAt;
+    public Guid UserId { get; set; }
 
-    public required DateTime Date
-    {
-        get => _date;
-        set => _date = value;
-    }
-
-    public required DateTime CreatedAt
-    {
-        get => _createdAt;
-        set => _createdAt = value;
-    }
 }
