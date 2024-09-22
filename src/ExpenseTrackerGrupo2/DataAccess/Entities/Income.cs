5bf9d665379ee5a0628596d972581e7aa9b8a2b6
salet-gutierrez-jalafund <salet.gutierrez@fundacion-jala.org>
2024-09-22 17:33:40 -0400

    refactor: add attributes of the Income entity

--- src/ExpenseTrackerGrupo2/DataAccess/Entities/Income.cs
+++ src/ExpenseTrackerGrupo2/DataAccess/Entities/Income.cs
@@ -2,41 +2,11 @@ namespace ExpenseTrackerGrupo2.src.ExpenseTrackerGrupo2.DataAccess.Entities;
 
 public class Income
 {
-    private Guid _id;
-    private Guid _userId;
-    private decimal _amount;
-    private DateTime _date;
-    private DateTime _createdAt;
-
-    public required Guid Id
-    {
-        get => _id;
-        set => _id = value;
-    }
-
-    public required Guid UserId
-    {
-        get => _userId;
-        set => _userId = value;
-    }
-
-    public required decimal Amount
-    {
-        get => _amount;
-        set => _amount = value;
-    }
-
+    private Guid Id { get; init; } = Guid.NewGuid();
+    public required decimal Amount { get; set; }
     public required string Source { get; set; }
+    public required DateTime Date { get; set; }
+    private DateTime CreatedAt { get; set; }
+    private Guid UserId { get; set; }
 
-    public required DateTime Date
-    {
-        get => _date;
-        set => _date = value;
-    }
-
-    public required DateTime CreatedAt
-    {
-        get => _createdAt;
-        set => _createdAt = value;
-    }
 }
