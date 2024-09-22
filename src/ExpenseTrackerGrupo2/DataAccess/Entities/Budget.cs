18f79e48c5cd84d45d96ca0d140f1edc390dc1b2
salet-gutierrez-jalafund <salet.gutierrez@fundacion-jala.org>
2024-09-22 17:32:24 -0400

    refactor: add attributes of the Budget entity

--- src/ExpenseTrackerGrupo2/DataAccess/Entities/Budget.cs
+++ src/ExpenseTrackerGrupo2/DataAccess/Entities/Budget.cs
@@ -2,27 +2,9 @@ namespace ExpenseTrackerGrupo2.src.ExpenseTrackerGrupo2.DataAccess.Entities;
 
 public class Budget
 {
-    private Guid _id;
-    private Guid _userId;
-    private decimal _budgetAmount;
+    public Guid Id { get; init; } = Guid.NewGuid();
+    public required decimal BudgetAmount { get; set; }
+    public required DateTime Month { get; set; }
+    public Guid UserId { get; set; }
 
-    public required Guid Id
-    {
-        get => _id;
-        set => _id = value;
-    }
-
-    public required Guid UserId
-    {
-        get => _userId;
-        set => _userId = value;
-    }
-
-    public required string Month { get; set; }
-
-    public required decimal BudgetAmount
-    {
-        get => _budgetAmount;
-        set => _budgetAmount = value;
-    }
 }
