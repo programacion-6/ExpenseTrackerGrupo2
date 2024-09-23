c1afa801e729039a9f8b17127fee093b1fea870d
Alex Paca <fujorita772@gmail.com>
2024-09-22 20:10:26 -0400

    fix: deleting comments

--- src/ExpenseTrackerGrupo2/Persistence/Database/Scripts/init.sql
+++ src/ExpenseTrackerGrupo2/Persistence/Database/Scripts/init.sql
@@ -1,4 +1,3 @@
--- User
 CREATE TABLE IF NOT EXISTS Users (
     Id SERIAL PRIMARY KEY,
     Name VARCHAR(100) NOT NULL,
@@ -7,7 +6,6 @@ CREATE TABLE IF NOT EXISTS Users (
     CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- Expense
 CREATE TABLE IF NOT EXISTS Expenses (
     Id SERIAL PRIMARY KEY,
     UserId INT NOT NULL,
@@ -22,7 +20,6 @@ CREATE TABLE IF NOT EXISTS Expenses (
     FOREIGN KEY (UserId) REFERENCES Users(Id)
 );
 
--- Income
 CREATE TABLE IF NOT EXISTS Incomes (
     Id SERIAL PRIMARY KEY,
     UserId INT NOT NULL,
@@ -32,7 +29,6 @@ CREATE TABLE IF NOT EXISTS Incomes (
     FOREIGN KEY (UserId) REFERENCES Users(Id)
 );
 
--- Budgets
 CREATE TABLE IF NOT EXISTS Budgets (
     Id SERIAL PRIMARY KEY,
     UserId INT NOT NULL,
@@ -41,7 +37,6 @@ CREATE TABLE IF NOT EXISTS Budgets (
     FOREIGN KEY (UserId) REFERENCES Users(Id)
 );
 
--- Goal
 CREATE TABLE IF NOT EXISTS Goals (
     Id SERIAL PRIMARY KEY,
     UserId INT NOT NULL,
@@ -51,35 +46,30 @@ CREATE TABLE IF NOT EXISTS Goals (
     FOREIGN KEY (UserId) REFERENCES Users(Id)
 );
 
--- Insert Users
 INSERT INTO Users (Name, Email, PasswordHash, CreatedAt) 
 VALUES 
 ('Juan Pérez', 'juan.perez@gmail.com', 'hashed_password_juan', CURRENT_TIMESTAMP),
 ('Ana Gómez', 'ana.gomez@gmail.com', 'hashed_password_ana', CURRENT_TIMESTAMP),
 ('Luis Martínez', 'luis.martinez@gmail.com', 'hashed_password_luis', CURRENT_TIMESTAMP);
 
--- Insert Expenses
 INSERT INTO Expenses (UserId, Amount, Description, Source, Month, GoalAmount, Category, Date, CreatedAt) 
 VALUES 
 (1, 100.50, 'Compra de supermercado', 'Tarjeta de crédito', 9, 200.00, 'Comida', '2024-09-15', CURRENT_TIMESTAMP),
 (2, 50.00, 'Pago de servicios públicos', 'Transferencia bancaria', 9, 100.00, 'Servicios', '2024-09-14', CURRENT_TIMESTAMP),
 (3, 200.00, 'Compra de ropa', 'Efectivo', 9, 300.00, 'Ropa', '2024-09-13', CURRENT_TIMESTAMP);
 
--- Insert Incomes
 INSERT INTO Incomes (UserId, Amount, Date, CreatedAt) 
 VALUES 
 (1, 1500.00, '2024-09-01', CURRENT_TIMESTAMP),
 (2, 1800.00, '2024-09-05', CURRENT_TIMESTAMP),
 (3, 1200.00, '2024-09-10', CURRENT_TIMESTAMP);
 
--- Insert Budgets
 INSERT INTO Budgets (UserId, BudgetAmount, CreatedAt) 
 VALUES 
 (1, 1200.00, CURRENT_TIMESTAMP),
 (2, 1500.00, CURRENT_TIMESTAMP),
 (3, 1000.00, CURRENT_TIMESTAMP);
 
--- Insert Goals
 INSERT INTO Goals (UserId, Deadline, CurrentAmount, CreatedAt) 
 VALUES 
 (1, '2024-12-31', 500.00, CURRENT_TIMESTAMP),
