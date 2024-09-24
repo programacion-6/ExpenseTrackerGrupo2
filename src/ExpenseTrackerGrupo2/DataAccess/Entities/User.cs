4cc45609a65ef51111964f1f0cb438f09b0a2c73
José Luis Terán Rocha <jose.luis.teran.rocha.2@gmail.com>
2024-09-24 00:36:17 -0400

    refactor: avoid non-nullable properties

--- src/ExpenseTrackerGrupo2/DataAccess/Entities/User.cs
+++ src/ExpenseTrackerGrupo2/DataAccess/Entities/User.cs
@@ -7,8 +7,8 @@ public class User
     public required string Email { get; set; }
     public required string Password { get; set; }
     public DateTime CreatedAt { get; set; }
-    public IList<Expense> Expenses { get; set; }
-    public IList<Income> Incomes { get; set; }
-    public IList<Budget> Budgets { get; set; }
-    public IList<Goal> Goals { get; set; }
+    public IList<Expense>? Expenses { get; set; }
+    public IList<Income>? Incomes { get; set; }
+    public IList<Budget>? Budgets { get; set; }
+    public IList<Goal>? Goals { get; set; }
 }
