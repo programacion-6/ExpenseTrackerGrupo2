463f74ed475b8423483be4ea4f6d031961c86aae
gabygabylagaby <gabrielagv7656@gmail.com>
2024-09-24 08:52:46 -0400

    feat: add method for the querys

--- src/ExpenseTrackerGrupo2/Utils/StringUtils.cs
+++ src/ExpenseTrackerGrupo2/Utils/StringUtils.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using System.Text.RegularExpressions;
 
 namespace ExpenseTrackerGrupo2.src.ExpenseTrackerGrupo2.Utils;
@@ -6,6 +7,24 @@ public static class StringUtils
     public static string ToSnakeCase(string input)
     {
         return Regex.Replace(input, "(?<!^)[A-Z]", "_$0").ToLower();
+    }
+
+    public static string GetColumnNames<T>()
+    {
+        var properties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+        return string.Join(", ", properties.Select(p => StringUtils.ToSnakeCase(p.Name)));
+    }
+
+    public static string GetColumnParameters<T>()
+    {
+        var properties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+        return string.Join(", ", properties.Select(p => "@" + StringUtils.ToSnakeCase(p.Name)));
+    }
+
+    public static string GetSetClause<T>()
+    {
+        var properties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+        return string.Join(", ", properties.Select(p => $"{StringUtils.ToSnakeCase(p.Name)} = @{StringUtils.ToSnakeCase(p.Name)}"));
     }
 
 }
