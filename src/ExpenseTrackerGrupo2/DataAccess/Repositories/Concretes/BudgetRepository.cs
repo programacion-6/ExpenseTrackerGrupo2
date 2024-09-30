609489f34594276f043d60116f1e9175844b3bb8
gabygabylagaby <gabrielagv7656@gmail.com>
2024-09-30 10:52:42 -0400

    fix: warnings null in budget

--- src/ExpenseTrackerGrupo2/DataAccess/Repositories/Concretes/BudgetRepository.cs
+++ src/ExpenseTrackerGrupo2/DataAccess/Repositories/Concretes/BudgetRepository.cs
@@ -8,19 +8,23 @@ public class BudgetRepository : BaseRepository<Budget>, IBudgetRepository
 {
     public BudgetRepository(IDbConnectionFactory dbConnectionFactory) : base(dbConnectionFactory) { }
 
-       public async Task<bool> SetBudget(Budget budget)
-        {
-            using var connection = await _dbConnectionFactory.CreateConnectionAsync();
-            var query = "INSERT INTO budget (budget_id, user_id, month, year, amount) VALUES (@BudgetId, @UserId, @Month, @Year, @Amount)";
-            var rowsAffected = await connection.ExecuteAsync(query, budget);
-            return rowsAffected > 0;
-        }
+    public async Task<bool> SetBudget(Budget budget)
+    {
+        using var connection = await _dbConnectionFactory.CreateConnectionAsync();
+        var query = "INSERT INTO budget (budget_id, user_id, month, year, amount) VALUES (@BudgetId, @UserId, @Month, @Year, @Amount)";
+        var rowsAffected = await connection.ExecuteAsync(query, budget);
+        return rowsAffected > 0;
+    }
 
-        public async Task<Budget> GetBudgetByUserIdAndMonth(Guid userId, int month, int year)
-        {
-            using var connection = await _dbConnectionFactory.CreateConnectionAsync();
-            var query = "SELECT * FROM budget WHERE user_id = @UserId AND month = @Month AND year = @Year";
-            var budget = await connection.QuerySingleOrDefaultAsync<Budget>(query, new { UserId = userId, Month = month, Year = year });
-            return budget;
-        }
+    public async Task<Budget> GetBudgetByUserIdAndMonth(Guid userId, int month, int year)
+    {
+        using var connection = await _dbConnectionFactory.CreateConnectionAsync();
+        var query = "SELECT * FROM budget WHERE user_id = @UserId AND month = @Month AND year = @Year";
+        var budget = await connection.QuerySingleOrDefaultAsync<Budget>(query, new { UserId = userId, Month = month, Year = year });
+
+        if (budget == null)
+            throw new InvalidOperationException("Budget not found for the specified user, month, and year.");
+
+        return budget;
+    }
 }
