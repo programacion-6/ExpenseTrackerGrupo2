2a570a104e31873fe9812150b8c0b42342e43b25
gabygabylagaby <gabrielagv7656@gmail.com>
2024-09-30 10:31:52 -0400

    feat: controller entities budget

---
+++ src/ExpenseTrackerGrupo2/DataAccess/Repositories/Concretes/BudgetRepository.cs
@@ -0,0 +1,26 @@
+using Dapper;
+using ExpenseTrackerGrupo2.Persistence.Database;
+using ExpenseTrackerGrupo2.DataAccess.Entities;
+
+namespace ExpenseTrackerGrupo2.DataAccess.Concretes;
+
+public class BudgetRepository : BaseRepository<Budget>, IBudgetRepository
+{
+    public BudgetRepository(IDbConnectionFactory dbConnectionFactory) : base(dbConnectionFactory) { }
+
+       public async Task<bool> SetBudget(Budget budget)
+        {
+            using var connection = await _dbConnectionFactory.CreateConnectionAsync();
+            var query = "INSERT INTO budget (budget_id, user_id, month, year, amount) VALUES (@BudgetId, @UserId, @Month, @Year, @Amount)";
+            var rowsAffected = await connection.ExecuteAsync(query, budget);
+            return rowsAffected > 0; // Return true if the insert was successful
+        }
+
+        public async Task<Budget> GetBudgetByUserIdAndMonth(Guid userId, int month, int year)
+        {
+            using var connection = await _dbConnectionFactory.CreateConnectionAsync();
+            var query = "SELECT * FROM budget WHERE user_id = @UserId AND month = @Month AND year = @Year";
+            var budget = await connection.QuerySingleOrDefaultAsync<Budget>(query, new { UserId = userId, Month = month, Year = year });
+            return budget;
+        }
+}
---
+++ src/ExpenseTrackerGrupo2/DataAccess/Repositories/Interfaces/IBudgetRepository.cs
@@ -0,0 +1,10 @@
+using ExpenseTrackerGrupo2.DataAccess.Entities;
+
+namespace ExpenseTrackerGrupo2.DataAccess.Concretes
+{
+    public interface IBudgetRepository : IBaseRepository<Budget>
+    {
+        Task<bool> SetBudget(Budget budget);
+        Task<Budget> GetBudgetByUserIdAndMonth(Guid userId, int month, int year);
+    }
+}
